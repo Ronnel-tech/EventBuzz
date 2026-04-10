208c44cb9a93dbca552dc2b73fddff1ef0e76a0b
Ronnel <logdatronnell@gmail.com>
2026-04-10 19:54:31 +0800

    49% done with the landing page

--- app/views/landing.php
+++ app/views/landing.php
@@ -7,8 +7,13 @@
     <title>EventBuzz</title>
     <link rel="icon" href="../public/assets/images/logo.png" type="image/x-icon">
 </head>
-<body class="p-10 pl-50 pr-50 w-full h-screen bg-cover bg-center">
-    <nav class="bg-surface flex justify-between pl-5 pr-5 card rounded-full sticky top-0 z-10 shadow-soft p-3 ">
+<body class="p-10 pl-50 pr-50 w-full h-screen">
+
+
+
+<div class="bg-[url('/public/assets/images/bg_img.jpg')] bg-cover bg-center p-5 h-5/6 rounded-4xl divider rounded-b-none  outline outline-offset-2 outline-[#2a2a2e] shadow-soft mb-1">
+    <nav class="bg-surface flex justify-between pl-5 pr-5 card rounded-full sticky top-5 z-10  shadow-soft p-3 bg-surface-hover ">
+
         <div class="flex items-center">
             <img src="../public/assets/images/logo.png" alt="EventBuzz Logo" class="size-7">
             <h4>EventBuzz</h4>
@@ -18,9 +23,46 @@
             <a href="">Signup</a>
             <h3>|</h3>
             <a href="">Login</a>
-        </div>
+        </div> 
     </nav>
 
+    <h1 class="pt-90  pl-50 text-black blur-[2px] ">Organizer create, <br> Organizer connect.</h1>
+</div>  
+
+<section class="overflow-hidden whitespace-nowrap bg-amber-50 p-7">
+  <div class="marquee text-4xl font-bold text-primary  bg-surface">
+    <div class="marquee-group" aria-hidden="true">
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span> EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+      <span>EVENTBUZZ</span>
+    </div>
+  </div>
+</section>
+
+<section class="flex justify-around align-center pt-20 pb-20 bg-surface outline outline-offset-2 outline-[#2a2a2e] shadow-soft mt-2">
+
+    <h1 class="text-[200px] leading-38">Event<br>Buzz</h1>
+    <hr class="transform rotate-180 w-1 h-90 bg-white  rounded-full">
+    <p class="pt-15 text-xl leading-normal pr-20 text-primary">EventBuzz is a modern <br> platform for managing and <br> promoting events. <br>Organizers can create and <br> manage events in one place, <br>while attendees can discover <br> events and secure their tickets <br> effortlessly.</p>
+
+</section >
+
+<section class="bg-white h-2 w-full flex justify-center items-center mt-1">
+    <button class="btn btn-primary rounded-full z-1">Get Started</button>
+</section>
 
 
 </body>
