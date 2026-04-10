89418b9cd236b7105aeb4b1d59a72620a5aa95d2
Ronnel <logdatronnell@gmail.com>
2026-04-10 20:42:42 +0800

    addded a new section on the landing page

--- app/views/landing.php
+++ app/views/landing.php
@@ -10,7 +10,7 @@
 <body class="p-10 pl-50 pr-50 w-full h-screen">
 
 
-
+<!-- -------------------------------------------------------------------------------------------------------------------------------- -->
 <div class="bg-[url('/public/assets/images/bg_img.jpg')] bg-cover bg-center p-5 h-5/6 rounded-4xl divider rounded-b-none  outline outline-offset-2 outline-[#2a2a2e] shadow-soft mb-1">
     <nav class="bg-surface flex justify-between pl-5 pr-5 card rounded-full sticky top-5 z-10  shadow-soft p-3 bg-surface-hover ">
 
@@ -28,7 +28,11 @@
 
     <h1 class="pt-90  pl-50 text-black blur-[2px] ">Organizer create, <br> Organizer connect.</h1>
 </div>  
+<!-- -------------------------------------------------------------------------------------------------------------------------------- -->
 
+
+
+<!-- -------------------------------------------------------------------------------------------------------------------------------- -->
 <section class="overflow-hidden whitespace-nowrap bg-amber-50 p-7">
   <div class="marquee text-4xl font-bold text-primary  bg-surface">
     <div class="marquee-group" aria-hidden="true">
@@ -51,7 +55,11 @@
     </div>
   </div>
 </section>
+<!-- -------------------------------------------------------------------------------------------------------------------------------- -->
 
+
+
+<!-- -------------------------------------------------------------------------------------------------------------------------------- -->
 <section class="flex justify-around align-center pt-20 pb-20 bg-surface outline outline-offset-2 outline-[#2a2a2e] shadow-soft mt-2">
 
     <h1 class="text-[200px] leading-38">Event<br>Buzz</h1>
@@ -59,11 +67,55 @@
     <p class="pt-15 text-xl leading-normal pr-20 text-primary">EventBuzz is a modern <br> platform for managing and <br> promoting events. <br>Organizers can create and <br> manage events in one place, <br>while attendees can discover <br> events and secure their tickets <br> effortlessly.</p>
 
 </section >
+<!-- -------------------------------------------------------------------------------------------------------------------------------- -->
 
+
+
+<!-- -------------------------------------------------------------------------------------------------------------------------------- -->
 <section class="bg-white h-2 w-full flex justify-center items-center mt-1">
     <button class="btn btn-primary rounded-full z-1">Get Started</button>
 </section>
+<!-- -------------------------------------------------------------------------------------------------------------------------------- -->
 
+
+
+
+<!-- -------------------------------------------------------------------------------------------------------------------------------- -->
+<section class="relative w-full overflow-hidden shadow-soft outline outline-offset-2 outline-[#2a2a2e] mt-1">
+
+    <!-- Blurred Background -->
+    <div class="absolute inset-0 bg-[url('/public/assets/images/gradient_bg.jpg')] 
+                bg-cover bg-center blur-3xl scale-110">
+    </div>
+
+    <!-- Content (NOT blurred) -->
+    <div class="relative flex flex-col items-center justify-center text-center py-25">
+
+        <div class="w-full flex justify-center pb-5">
+            <img src="../public/assets/images/logo.png" alt="EventBuzz Logo" class="w-32 h-32 ">
+        </div>
+
+        <p class="text-lg text-primary">
+            EventBuzz brings organizers and attendees together through a platform <br>
+            designed to simplify event promotion, ticketing, and discovery.
+        </p>
+
+    </div>
+
+</section>
+<!-- -------------------------------------------------------------------------------------------------------------------------------- -->
+
+
+
+<!-- -------------------------------------------------------------------------------------------------------------------------------- -->
+
+
+<section>
+
+</section>
+
+
+ <!-- -------------------------------------------------------------------------------------------------------------------------------- -->
 
 </body>
 </html>
