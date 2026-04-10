ce2313aabb9045dbd6143fbe3db7720f101695a9
Ronnel <logdatronnell@gmail.com>
2026-04-10 10:17:46 +0800

    fixed the design system

--- app/views/landing.php
+++ app/views/landing.php
@@ -7,11 +7,22 @@
     <title>EventBuzz</title>
     <link rel="icon" href="../public/assets/images/logo.png" type="image/x-icon">
 </head>
-<body class="bg-red-500">
+<body class="p-10 pl-50 pr-50 w-full h-screen bg-cover bg-center">
+    <nav class="bg-surface flex justify-between pl-5 pr-5 card rounded-full sticky top-0 z-10 shadow-soft p-3 ">
+        <div class="flex items-center">
+            <img src="../public/assets/images/logo.png" alt="EventBuzz Logo" class="size-7">
+            <h4>EventBuzz</h4>
+        </div>
 
-<div class="w-full h-screen bg-red-500">
-    <h1 class="text-3xl font-bold text-center text-red-500">`EventBuzz`</h1>
-</div>
+        <div class="flex items-center gap-5">
+            <a href="">Signup</a>
+            <h3>|</h3>
+            <a href="">Login</a>
+        </div>
+    </nav>
+
+
+
 </body>
 </html>
 
